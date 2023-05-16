11930114d9fb62f445a3623df2b4d4842f6d53fb
Lottipopps <122055716+Lottipopps@users.noreply.github.com>
2023-05-16 19:58:58 +0200

    Updated OtherInfo.js

--- my-app/src/OtherInfo.js
+++ my-app/src/OtherInfo.js
@@ -1,2 +1,37 @@
 import React from "react";
 import "./styles.css";
+
+function OtherInfo() {
+  return (
+    <div className="col-3">
+      <ul>
+        <li id="date">
+          <span role="img" aria-label="calendar">
+            🗓️
+          </span>{" "}
+          Wednesday 22 Feb 2023, 00:37
+        </li>
+        <li>
+          <span role="img" aria-label="globe">
+            🌍
+          </span>{" "}
+          <span id="weather-description">Overcast Clouds </span>
+        </li>
+        <li>
+          <span role="img" aria-label="wind">
+            💨
+          </span>{" "}
+          Wind speed <span id="wind-speed">2</span> km/h
+        </li>
+        <li>
+          <span role="img" aria-label="bubbles">
+            🫧
+          </span>{" "}
+          Humidity <span id="humidity">91</span>%
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default OtherInfo;
