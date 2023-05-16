3a88048f0fdf824cb4909e9cc447086c6c519eaf
Lottipopps <122055716+Lottipopps@users.noreply.github.com>
2023-05-16 20:00:33 +0200

    Updated Today.js

--- my-app/src/Today.js
+++ my-app/src/Today.js
@@ -1,2 +1,19 @@
 import React from "react";
 import "./styles.css";
+
+function Today() {
+  return (
+    <div className="col-6">
+      <div className="row">
+        <div className="d-flex icon-temperature">
+          <img src="" alt="" id="weather-icon" width="170px" />
+          <div>
+            <span id="current-temperature"> 6</span>{" "}
+            <span className="conversion-btns">°C</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Today;
