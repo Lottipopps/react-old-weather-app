697864bc0a45e08e06117fb936eac0b5cf2f3216
Lottipopps <122055716+Lottipopps@users.noreply.github.com>
2023-05-16 19:49:44 +0200

    Added content to Footer

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -1,2 +1,20 @@
 import React from "react";
 import "./styles.css";
+
+function Footer() {
+  return (
+    <footer>
+      {" "}
+      <a
+        href="https://github.com/Lottipopps/Vanilla-JS-Weather-APP-Carlotta"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Open source code
+      </a>{" "}
+      by Carlotta Missiroli 🎶
+    </footer>
+  );
+}
+
+export default Footer;
