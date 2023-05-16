16aab99ac9041cfc8d844bb3aa437cbf2097e0cf
Lottipopps <122055716+Lottipopps@users.noreply.github.com>
2023-05-16 19:50:37 +0200

    Update Forecast.js

--- my-app/src/Forecast.js
+++ my-app/src/Forecast.js
@@ -1,2 +1,22 @@
 import React from "react";
 import "./styles.css";
+import Today from "./Today";
+import OtherInfo from "./OtherInfo";
+import NextWeek from "./NextWeek";
+
+function Forecast() {
+  return (
+    <div className="Forecast">
+      <div className="row">
+        <h1>London</h1>
+      </div>
+      <div className="row ">
+        <Today />
+        <OtherInfo />
+        <hr />
+        <NextWeek />
+      </div>
+    </div>
+  );
+}
+export default Forecast;
