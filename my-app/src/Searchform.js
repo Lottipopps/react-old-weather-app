47c90f54e8f7d09e33e8ba8f62415db90360407e
Lottipopps <122055716+Lottipopps@users.noreply.github.com>
2023-05-16 19:59:46 +0200

    Updated SearchForm.js

--- my-app/src/Searchform.js
+++ my-app/src/Searchform.js
@@ -1,2 +1,34 @@
 import React from "react";
 import "./styles.css";
+
+function Searchform() {
+  return (
+    <form id="search-form" autoComplete="off">
+      <div className="row">
+        <div className="col-8">
+          <input
+            type="search"
+            placeholder="Search for a city"
+            className="form-control"
+            id="city-search"
+          />
+        </div>
+        <div className="col-4">
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-primary btn-md"
+          />
+          <input
+            type="button"
+            value="Current Location"
+            id="current-location"
+            className="btn btn-success btn-sm"
+          />
+        </div>
+      </div>
+    </form>
+  );
+}
+
+export default Searchform;
